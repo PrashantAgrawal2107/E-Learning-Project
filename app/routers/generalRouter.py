52dc1b6febd80cdb0b07f80df105803a547cd77b
PrashantAgrawal2107 <prashantagrawal2107@gmail.com>
2025-08-22 15:06:34 +0530

    general route correction

--- app/routers/generalRouter.py
+++ app/routers/generalRouter.py
@@ -3,10 +3,13 @@ from sqlalchemy.orm import Session
 from ..core.dbConfig import get_db
 from ..services import instructorServices, studentServices
 from ..auth.authentication import get_current_user
+from typing import Union
+from ..schemas.studentSchema import StudentResponse
+from ..schemas.instructorSchema import InstructorResponse
 
 router = APIRouter(prefix="/general", tags=["General"])
 
-@router.get("/profile")
+@router.get("/profile", response_model=Union[StudentResponse , InstructorResponse])
 def get_profile(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
     if current_user.role == 'instructor':
         return instructorServices.get_instructor_by_id(current_user.id , db)
