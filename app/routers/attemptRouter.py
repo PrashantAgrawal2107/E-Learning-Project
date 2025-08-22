25bd62023fe33e1e6e15063798fff57d3af7c5fb
Prashant Agrawal <prashantagrawal2107@gmail.com>
2025-08-22 21:41:08 +0530

    attempt apis updated

--- app/routers/attemptRouter.py
+++ app/routers/attemptRouter.py
@@ -5,6 +5,7 @@ from ..schemas import attemptSchema
 from ..services import attemptServices
 from ..auth.authentication import get_current_user, require_role
 from ..models.studentModel import Student
+from ..schemas.sortSchema import SortSchema
 
 router = APIRouter(prefix="/attempts", tags=["Attempts"])
 
@@ -20,20 +21,20 @@ def delete_attempt(attempt_id: int, db: Session = Depends(get_db), current_user:
 
 
 @router.get("/", response_model=list[attemptSchema.AttemptResponse])
-def get_all_attempts(db: Session = Depends(get_db)):
-    return attemptServices.get_all_attempts(db)
+def get_all_attempts(db: Session = Depends(get_db), current_user = Depends(get_current_user), params : SortSchema = Depends()):
+    return attemptServices.get_all_attempts(db, current_user, params)
 
 
 @router.get("/{attempt_id}", response_model=attemptSchema.AttemptResponse)
-def get_attempt_by_id(attempt_id: int, db: Session = Depends(get_db)):
-    return attemptServices.get_attempt_by_id(db, attempt_id)
+def get_attempt_by_id(attempt_id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return attemptServices.get_attempt_by_id(db, attempt_id, current_user)
 
 
 @router.get("/student/{student_id}", response_model=list[attemptSchema.AttemptResponse])
-def get_attempts_of_student(student_id: int, db: Session = Depends(get_db)):
-    return attemptServices.get_attempts_of_student(db, student_id)
+def get_attempts_of_student(student_id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return attemptServices.get_attempts_of_student(db, student_id, current_user)
 
 
 @router.get("/quiz/{quiz_id}", response_model=list[attemptSchema.AttemptResponse])
-def get_attempts_on_quiz(quiz_id: int, db: Session = Depends(get_db)):
-    return attemptServices.get_attempts_on_quiz(db, quiz_id)
+def get_attempts_on_quiz(quiz_id: int, db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    return attemptServices.get_attempts_on_quiz(db, quiz_id, current_user)
--- app/services/attemptServices.py
+++ app/services/attemptServices.py
@@ -5,16 +5,20 @@ from ..schemas import attemptSchema, attemptAnswerSchema
 from ..models.studentModel import Student
 from ..models.quizModel import Quiz
 from ..models import moduleModel, courseModel
+from ..models.enrollmentModel import Enrollment
+from ..schemas import sortSchema
+from ..auth.validations import sort_validation
+from ..models.moduleModel import Module
+from ..models.courseModel import Course
 
 def create_attempt(db: Session, attempt_data: attemptSchema.AttemptCreate, current_user: Student):
-    
     student = db.query(studentModel.Student).filter(studentModel.Student.id == attempt_data.student_id).first()
     if not student:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Student with id {attempt_data.student_id} not found"
         )
-    
+
     if current_user.id != attempt_data.student_id:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -28,20 +32,49 @@ def create_attempt(db: Session, attempt_data: attemptSchema.AttemptCreate, curre
             detail=f"Quiz with id {attempt_data.quiz_id} not found"
         )
 
+    enrollment = (
+        db.query(Enrollment)
+        .filter(
+            Enrollment.student_id == attempt_data.student_id,
+            Enrollment.course_id == quiz.module.course_id
+        )
+        .first()
+    )
+    if not enrollment:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You are not enrolled in the course of this quiz"
+        )
+
+    # ✅ Check number of attempts
+    attempt_count = (
+        db.query(attemptModel.Attempt)
+        .filter(
+            attemptModel.Attempt.student_id == attempt_data.student_id,
+            attemptModel.Attempt.quiz_id == attempt_data.quiz_id
+        )
+        .count()
+    )
+
+    if attempt_count >= 3:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="You have already attempted this quiz 3 times. No more attempts allowed."
+        )
+
+    # ✅ New Attempt
     attempt = attemptModel.Attempt(
         student_id=attempt_data.student_id,
         quiz_id=attempt_data.quiz_id,
         score=0
     )
     db.add(attempt)
-    db.commit()
-    db.refresh(attempt)
+    db.flush()
 
     total_score = 0
 
     if hasattr(attempt_data, "answers") and attempt_data.answers:
         for ans in attempt_data.answers:
-
             question = (
                 db.query(questionModel.Question)
                 .filter(
@@ -50,7 +83,6 @@ def create_attempt(db: Session, attempt_data: attemptSchema.AttemptCreate, curre
                 )
                 .first()
             )
-
             if not question:
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
@@ -65,7 +97,6 @@ def create_attempt(db: Session, attempt_data: attemptSchema.AttemptCreate, curre
                 )
                 .first()
             )
-
             if not selected_option:
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
@@ -80,7 +111,6 @@ def create_attempt(db: Session, attempt_data: attemptSchema.AttemptCreate, curre
                 )
                 .first()
             )
-
             if not correct_option:
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST,
@@ -88,9 +118,8 @@ def create_attempt(db: Session, attempt_data: attemptSchema.AttemptCreate, curre
                 )
 
             is_correct = ans.selected_option_id == correct_option.id
-
             if is_correct:
-                total_score += 1 
+                total_score += 1
 
             answer = attemptAnswerModel.AttemptAnswer(
                 attempt_id=attempt.id,
@@ -99,9 +128,6 @@ def create_attempt(db: Session, attempt_data: attemptSchema.AttemptCreate, curre
                 is_correct=is_correct
             )
             db.add(answer)
-
-
-        db.commit()
 
     attempt.score = total_score
     db.commit()
@@ -129,35 +155,152 @@ def delete_attempt(db: Session, attempt_id: int, current_user: Student):
     return {"message": f"Attempt {attempt_id} deleted successfully"}
 
 
-def get_all_attempts(db: Session):
-    return db.query(attemptModel.Attempt).all()
+def get_all_attempts(db: Session, current_user, params: sortSchema):
+    query = db.query(attemptModel.Attempt).join(attemptModel.Attempt.quiz).join(Quiz.module)
+
+    if current_user.role == "instructor":
+        query = query.join(Module.course).filter(Course.instructor_id == current_user.id)
+
+    elif current_user.role == "student":
+        enrolled_course_ids = [
+            e.course_id for e in db.query(Enrollment)
+                                  .filter(Enrollment.student_id == current_user.id)
+                                  .all()
+        ]
+        query = query.filter(Module.course_id.in_(enrolled_course_ids))
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Role not allowed to fetch attempts"
+        )
+
+    sort_by = params.sort_by
+    order = params.order
+    limit = params.limit
+    skip = params.skip
+
+    valid_sort_fields = {
+        "id": attemptModel.Attempt.id,
+        "quiz_id": attemptModel.Attempt.quiz_id,
+        "score": attemptModel.Attempt.score,
+        "created_on": attemptModel.Attempt.created_on
+    }
+
+    sort = sort_validation(valid_sort_fields,sort_by, order)
+
+    return query.order_by(sort).offset(skip * limit).limit(limit).all()
 
 
-def get_attempt_by_id(db: Session, attempt_id: int):
-    attempt = db.query(attemptModel.Attempt).filter(attemptModel.Attempt.id == attempt_id).first()
+
+def get_attempt_by_id(db: Session, attempt_id: int, current_user):
+    attempt = (
+        db.query(attemptModel.Attempt)
+        .join(attemptModel.Attempt.quiz)
+        .join(Quiz.module)
+        .join(Module.course)
+        .filter(attemptModel.Attempt.id == attempt_id)
+        .first()
+    )
+
     if not attempt:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Attempt with id {attempt_id} not found"
         )
+
+    if current_user.role == "instructor":
+        if attempt.quiz.module.course.instructor_id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to access this attempt"
+            )
+
+    elif current_user.role == "student":
+        enrolled_course_ids = [
+            e.course_id for e in db.query(Enrollment)
+                                  .filter(Enrollment.student_id == current_user.id)
+                                  .all()
+        ]
+        if attempt.quiz.module.course_id not in enrolled_course_ids:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to access this attempt"
+            )
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Role not allowed to fetch attempt"
+        )
+
     return attempt
 
-
-def get_attempts_of_student(db: Session, student_id: int):
+def get_attempts_of_student(db: Session, student_id: int, current_user):
     student = db.query(studentModel.Student).filter(studentModel.Student.id == student_id).first()
     if not student:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Student with id {student_id} not found"
         )
+
+    if current_user.role == "student":
+        if current_user.id != student_id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to view other student's attempts"
+            )
+
+    elif current_user.role == "instructor":
+        enrolled_course_ids = [
+            e.course_id for e in db.query(Enrollment)
+                                  .filter(Enrollment.student_id == student_id)
+                                  .all()
+        ]
+        instructor_course_ids = [
+            c.id for c in db.query(Course).filter(Course.instructor_id == current_user.id).all()
+        ]
+
+        if not any(c in instructor_course_ids for c in enrolled_course_ids):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to access this student's attempts"
+            )
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Role not allowed"
+        )
+
     return db.query(attemptModel.Attempt).filter(attemptModel.Attempt.student_id == student_id).all()
 
 
-def get_attempts_on_quiz(db: Session, quiz_id: int):
+
+def get_attempts_on_quiz(db: Session, quiz_id: int, current_user):
     quiz = db.query(quizModel.Quiz).filter(quizModel.Quiz.id == quiz_id).first()
     if not quiz:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Quiz with id {quiz_id} not found"
         )
-    return db.query(attemptModel.Attempt).filter(attemptModel.Attempt.quiz_id == quiz_id).all()
+
+    if current_user.role == "student":
+        return db.query(attemptModel.Attempt).filter(
+            attemptModel.Attempt.quiz_id == quiz_id,
+            attemptModel.Attempt.student_id == current_user.id
+        ).all()
+
+    elif current_user.role == "instructor":
+        if quiz.module.course.instructor_id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="You are not allowed to access attempts of this quiz"
+            )
+        return db.query(attemptModel.Attempt).filter(attemptModel.Attempt.quiz_id == quiz_id).all()
+
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Role not allowed"
+        )
